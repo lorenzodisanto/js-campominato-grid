71ef272418531486943db188a56b31c0640b807f
lorenzodisanto <lore.disanto95@gmail.com>
2024-01-18 17:56:51 +0100

    feat: add bonus

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,17 @@
 // funzione genero griglia
 function genGrid(container) {
   container.innerHTML = "";
+  let level = selectLevel.value;
+  let cellNumber;
+  if (level == "facile") {
+    cellNumber = 100;
+  } else if (level == "medio") {
+    cellNumber = 81;
+  } else if (level == "difficile") {
+    cellNumber = 49;
+  }
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < cellNumber; i++) {
     const cellElement = genCell(i + 1);
     container.append(cellElement);
   }
@@ -13,6 +22,14 @@ function genGrid(container) {
 function genCell(text) {
   const cell = document.createElement("div");
   cell.classList.add("box");
+  let level = selectLevel.value;
+  if (level == "facile") {
+    cell.classList.add("easy");
+  } else if (level == "medio") {
+    cell.classList.add("medium");
+  } else if (level == "difficile") {
+    cell.classList.add("hard");
+  }
   cell.innerText = text;
   cell.addEventListener("click", function () {
     cell.classList.toggle("active");
