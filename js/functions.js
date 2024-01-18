757369503cf876bd546a35e422b05a5f1320d00e
lorenzodisanto <lore.disanto95@gmail.com>
2024-01-18 16:28:21 +0100

    feat: add functions

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,22 @@
-console.log("JS functions ok");
+// funzione genero griglia
+function genGrid(container) {
+  container.innerHTML = "";
+
+  for (let i = 0; i < 100; i++) {
+    const cellElement = genCell(i + 1);
+    container.append(cellElement);
+  }
+}
+
+// funzione genero cella
+
+function genCell(text) {
+  const cell = document.createElement("div");
+  cell.classList.add("box");
+  cell.innerText = text;
+  cell.addEventListener("click", function () {
+    cell.classList.toggle("active");
+    console.log("hai clickato la cella " + text);
+  });
+  return cell;
+}
