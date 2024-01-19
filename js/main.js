2ffb4ae05076e7f86e0503bf62c0c6b97fb2b576
lorenzodisanto <lore.disanto95@gmail.com>
2024-01-19 16:07:47 +0100

    feat: add step 2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const gamingtableElement = document.getElementById("gamingtable");
 const playButton = document.getElementById("playbutton");
 const selectLevel = document.getElementById("selectlevel");
 
+const myBombs = [];
+
 playButton.addEventListener("click", function () {
   genGrid(gamingtableElement);
   gamingtableElement.classList.remove("d-none");
